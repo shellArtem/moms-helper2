2fbffb9177f6871266c729939b64b665f127414d
shellArtem <1shelldrunk1@gmail.com>
2025-08-21 11:42:31 +0300

    test

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,18 @@ const PORT = 3001; // Порт для бэкенда
 app.use(cors());
 app.use(bodyParser.json());
 
+const staticPath = path.join(__dirname, 'public/images');
+
+console.log("-----------------------------------------");
+console.log("СЕРВЕР ИЩЕТ КАРТИНКИ В ПАПКЕ:", staticPath);
+console.log("-----------------------------------------");
+
+app.use('/images', express.static(staticPath));
+// app.use('/images', express.static(path.join(__dirname, 'public/images')));
 // Роуты для нашего API
 app.use('/api', apiRoutes);
 
 // Обслуживание статических файлов (картинок)
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
