8eae2f84c981cdb8b5f761f31065ad10d0a0a4c7
shellArtem <1shelldrunk1@gmail.com>
2025-08-25 11:00:11 +0300

    test

--- nextjs/src/components/Metrika.js
+++ nextjs/src/components/Metrika.js
@@ -32,9 +32,9 @@ export const Metrika = () => {
                             m[i].l=1*new Date();
                             for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
                             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+                        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=103783435', 'ym');
                         
-                        ym(${METRIKA_ID}, 'init', {
+                        ym(103783435, 'init', {
                             ssr:true,
                             clickmap:true,
                             trackLinks:true,
@@ -47,11 +47,29 @@ export const Metrika = () => {
             />
             <noscript>
                 <div>
-                    <img src={`https://mc.yandex.ru/watch/${METRIKA_ID}`} style={{ position: 'absolute', left: '-9999px' }} alt="" />
+                    <img src={`https://mc.yandex.ru/watch/103783435`} style={{ position: 'absolute', left: '-9999px' }} alt="" />
                 </div>
             </noscript>
         </>
     );
 };
+
+
+// <!-- Yandex.Metrika counter -->
+// <script type="text/javascript">
+//     (function(m,e,t,r,i,k,a){
+//         m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+//         m[i].l=1*new Date();
+//         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+//         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+//     })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103783435', 'ym');
+
+//     ym(103783435, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+// </script>
+// <noscript><div><img src="https://mc.yandex.ru/watch/103783435" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+// <!-- /Yandex.Metrika counter -->
+
+
+
 
 
