0c1a18394c8f86d436a264dc4b62e9640b30ddba
shellArtem <1shelldrunk1@gmail.com>
2025-08-25 11:02:24 +0300

    test

--- nextjs/src/components/Metrika.js
+++ nextjs/src/components/Metrika.js
@@ -10,64 +10,24 @@ export const Metrika = () => {
     const pathname = usePathname();
     const searchParams = useSearchParams();
 
-    // Эффект для отслеживания SPA-переходов
     useEffect(() => {
         const url = `${pathname}${searchParams.toString() ? `?${searchParams.toString()}` : ''}`;
+        
         // @ts-ignore
         if (typeof window.ym === 'function') {
             // @ts-ignore
-            window.ym(METRIKA_ID, 'hit', url);
+            window.ym(METRIKA_ID, 'hit', url, {
+                params: {
+                    // Здесь можно передавать дополнительные параметры визита, если нужно
+                }
+            });
         }
     }, [pathname, searchParams]);
 
-    // Компонент будет рендерить скрипт и noscript напрямую
-    return (
-        <>
-            <script
-                type='text/javascript'
-                dangerouslySetInnerHTML={{
-                    __html: `
-                        (function(m,e,t,r,i,k,a){
-                            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                            m[i].l=1*new Date();
-                            for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
-                            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=103783435', 'ym');
-                        
-                        ym(103783435, 'init', {
-                            ssr:true,
-                            clickmap:true,
-                            trackLinks:true,
-                            accurateTrackBounce:true,
-                            webvisor:true,
-                            ecommerce:"dataLayer"
-                        });
-                    `,
-                }}
-            />
-            <noscript>
-                <div>
-                    <img src={`https://mc.yandex.ru/watch/103783435`} style={{ position: 'absolute', left: '-9999px' }} alt="" />
-                </div>
-            </noscript>
-        </>
-    );
+    return null;
 };
 
 
-// <!-- Yandex.Metrika counter -->
-// <script type="text/javascript">
-//     (function(m,e,t,r,i,k,a){
-//         m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-//         m[i].l=1*new Date();
-//         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-//         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-//     })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103783435', 'ym');
-
-//     ym(103783435, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-// </script>
-// <noscript><div><img src="https://mc.yandex.ru/watch/103783435" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-// <!-- /Yandex.Metrika counter -->
 
 
 
