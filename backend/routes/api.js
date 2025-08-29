aaad00aac11ab4aca20c88b0bcf355d954fda9dd
shellArtem <1shelldrunk1@gmail.com>
2025-08-29 10:14:53 +0300

    test

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,5 +1,6 @@
 // backend/routes/api.js
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
