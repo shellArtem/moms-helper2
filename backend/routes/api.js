62281cecc9e9dfd4bfffb13cc1d6632faffc6a20
shellArtem <1shelldrunk1@gmail.com>
2025-09-08 11:21:30 +0300

    fix pass

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -72,8 +72,7 @@ router.get('/articles/:slug', (req, res) => {
 // Вход в админку
 router.post('/admin/login', (req, res) => {
   const { password } = req.body;
-  console.log(process.env.ADMIN_PASSWORD)
-  if (password === '2361951') {
+  if (password === process.env.ADMIN_PASSWORD) {
     // В реальном приложении здесь будет генерация JWT токена
     res.json({ success: true, message: "Вход выполнен" });
   } else {
