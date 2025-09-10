ecbce811af2eba13752232ff8d3766ea359c8810
shellArtem <1shelldrunk1@gmail.com>
2025-09-10 10:39:13 +0300

    test

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps : [{
       name   : "moms-helper-backend", // Более осмысленное имя для вашего приложения
-      script : "./backend/server.js", // Путь к server.js относительно корня moms-helper2
-      env_file: "./backend/.env",     // Путь к .env файлу, если он находится в папке backend
+      script : "/server.js", // Путь к server.js относительно корня moms-helper2
+      env_file: "/.env",     // Путь к .env файлу, если он находится в папке backend
       // Если .env файл находится в корне moms-helper2, то env_file: ".env",
       // Более специфичные переменные окружения, если нужно переопределить
       // env: {
